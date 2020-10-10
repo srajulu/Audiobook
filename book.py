1f87b96f15acbd3a3435e00f64f9eda8b3774c1c
SWETHA RAJULU <55911880+srajulu@users.noreply.github.com>
2020-10-10 21:15:29 +0530

    Update book.py

--- book.py
+++ book.py
@@ -5,3 +5,7 @@ from tkinter.filedialog import *
 book = askopenfilename()
 pdfreader = PyPDF2.PdfFileReader(book)
 pages = pdfreader.numpages
+
+
+for num in range(0,pages):
+  page = pdfreader.getPage(num)
